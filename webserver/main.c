1bedb3a2b7117d5811a6cc75e51bcae4eb4f2b0d
Alexandre Courand <a.courand@hotmail.fr>
2016-02-10 16:06:26 +0100

    serveur_tcp_simple

--- webserver/main.c
+++ webserver/main.c
@@ -9,45 +9,36 @@
 
 int main(void){
 	int socket_serveur = creer_serveur(8080);
-	initialiser_signaux();
-	//if(fork()==0){
-while(1){
+	while(1){
 		int socket_client ;
 		socket_client = accept ( socket_serveur , NULL , NULL );
 		if ( socket_client == -1){
 			perror ( " accept " );
+			break;
 			/* traitement d ’ erreur */
 		}
 		/* On peut maintenant dialoguer avec le client */
 
-		const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n\n\n\n\n\n\n\n\n\n !\n" ; // les 10 lignes du serveur 
-		//différent que d'envoyer  10 fois le même message
-		//int i = 0;
-		//for(i = 0;i<10;i++){
-			//sleep(1);
-			
-			
-
-			
-			
-			write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
-			while(1){
-				char buf[1024];
-				int j=read(socket_client,buf,1024);
-				if ( j == -1){
-					perror ("read");
-				/* traitement d ’ erreur */
-				}
-				buf[j]='\0';
-				write ( socket_client , buf, strlen (buf));
-			
+		const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n" ;
+		sleep(1);
+		write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
+		
+		while(1){
+			char buf[1024];
+			int j=read(socket_client,buf,1024);
+			if(j==-1){
+				perror("read");
+				break;
+				/* traitement d'erreur */
 			}
-		//}
-			
-		
-		
-	//}
-}
+			if(j==0){
+				perror("deconnexion");
+				break;
+				/* traitement d'erreur */
+			}
+			write(socket_client,buf,j);
+		}
+	}
 	return 0;
 } 
 
